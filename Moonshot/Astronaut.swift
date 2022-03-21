c928fc172ea49de802807521ed604d20dc59fba0
Marcel-git666 <93435045+Marcel-git666@users.noreply.github.com>
2022-03-21 19:33:37 +0100

    Finished

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,17 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Marcel Mravec on 09.02.2022.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+    
+}
---
+++ Moonshot/MIssionScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollView.swift
+//  Moonshot
+//
+//  Created by Marcel Mravec on 25.02.2022.
+//
+
+import SwiftUI
+
+struct MissionScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionScrollView()
+    }
+}
